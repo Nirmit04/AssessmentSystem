a0282ab674620cc845f0cea0394c8ba980c8ab77
AnandRoy01 <anandroyyo@gmail.com>
2019-03-05 19:01:37 +0530

    Anand| Added Dashboard

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentCreatorComponent } from './content-creator/content-creator.component';
+import { DashboardComponent } from './content-creator/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, ContentCreatorComponent, DashboardComponent ],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
