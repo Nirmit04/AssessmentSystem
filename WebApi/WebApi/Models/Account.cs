2ab85ffa796e7fe5ea39743b959ccf97b251af2f
abhijeetanandshah6997 <abhijeet.shah@nineleaps.com>
2019-03-08 23:04:41 +0530

    Abhijeet | Model Modified

--- WebApi/WebApi/Models/Account.cs
+++ WebApi/WebApi/Models/Account.cs
@@ -8,26 +8,13 @@ namespace WebApi.Models
 {
     public class Account
     {   
-        [Key]
         public string Id { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Role { get; set; }
+        public string[] Role { get; set; }
         public string ImageURL { get; set; }
         public string GoogleId { get; set; }
-
-        public virtual ICollection<DetailedReport> DetailedReports { get; set; }
-        
-        public virtual ICollection<Quiz> Quizs { get; set; }
-       
-        public virtual ICollection<QuizSchedule> QuizSchedules { get; set; }
-        
-        public virtual ICollection<Report> Reports { get; set; }
-      
-        public virtual ICollection<Question> Questions { get; set; }
-
-        public virtual ICollection<Subject> Subjects { get; set; }
     }
 }
--- WebApi/WebApi/Models/IdentityModels.cs
+++ WebApi/WebApi/Models/IdentityModels.cs
@@ -13,7 +13,6 @@ namespace WebApi.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Role { get; set; }
         public string ImageURL { get; set; }
         public string GoogleId { get; set; }
 
