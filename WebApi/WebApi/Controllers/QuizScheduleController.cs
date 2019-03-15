e56f27b641bc1a21d7930bcbd7f230fe720704bf
abhijeetanandshah6997 <abhijeet.shah@nineleaps.com>
2019-03-15 12:33:34 +0530

    Abhijeet | Code Refactored & Model Update

--- WebApi/WebApi/Controllers/QuizScheduleController.cs
+++ WebApi/WebApi/Controllers/QuizScheduleController.cs
@@ -4,19 +4,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     public class QuizScheduleController : ApiController
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
-
-        [HttpPost]
-        [Route("api/Schedule")]
-        public IHttpActionResult ScheduleCreate(QuizScheduleController quizSchedule1)
-        {
-            return Ok();
-        }
     }
 }
