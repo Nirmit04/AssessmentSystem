2fecf5da9bb1e9365344717eccb2b458b4ffc7a9
pulkitvarma <pulkit.verma@nineleaps.com>
2019-03-22 12:45:33 +0530

    Pulkit:Doughnut Chart

--- WebApp/src/app/employee/detailed-report/detailed-report.component.ts
+++ WebApp/src/app/employee/detailed-report/detailed-report.component.ts
@@ -12,16 +12,30 @@ export class DetailedReportComponent implements OnInit {
   public doughnutChartLabels1 = ['Attempted', 'UnAttempted'];
   public doughnutChartData1 = [this.service.data.CorrectAnswers + this.service.data.WrongAnswers, this.service.data.UnattemptedAnswers];
   public doughnutChartType1 = 'doughnut';
+  public doughnutChartOptions1 = {
+    legend: {
+      onClick: function (e) {
+          e.stopPropagation();
+      }
+  }
+  }
 
-  quizname = 'Python';
+  quizname = this.service.data.QuizName;
   public doughnutChartLabels2 = ['Correct', 'InCorrect'];
   public doughnutChartData2 = [this.service.data.CorrectAnswers, this.service.data.WrongAnswers];
   public doughnutChartType2 = 'doughnut';
+  public doughnutChartOptions2 = {
+    legend: {
+      onClick: function (e) {
+          e.stopPropagation();
+      }
+  }
+  }
 
   gaugeType = 'semi';
-  gaugeValue = this.service.data.MarksScored;
-  gaugeLabel = 'Marks Scored';
-  gaugeAppendText = '/' + this.service.data.TotalMarks;
+  gaugeValue = ((this.service.data.MarksScored / this.service.data.TotalMarks) * 100).toPrecision(2).toString();
+  gaugeLabel = 'Performance';
+  gaugeAppendText = '%';
 
 
   constructor(private service: EmployeeService) { }
