a5c193cfac84b62411597ac11c7ee5069e59b737
Nirmit04 <nirmit.agarwal@nineleaps.com>
2019-03-25 18:46:51 +0530

    Nirmit

--- WebApp/src/app/reporting-user/analytics-by-user/view-user-details/view-user-details.component.ts
+++ WebApp/src/app/reporting-user/analytics-by-user/view-user-details/view-user-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ReportingUserService } from '../../shared/reporting-user.service';
+import { SingleDataSet, Label } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 @Component({
   selector: 'app-view-user-details',
   templateUrl: './view-user-details.component.html',
@@ -8,17 +10,33 @@ import { ReportingUserService } from '../../shared/reporting-user.service';
 })
 export class ViewUserDetailsComponent implements OnInit {
 
+  data1: any[];
+  public polarAreaChartLabels: Label[] = ['Highest-Score', 'Lowest Score', 'Accuracy', 'Average-Score'];
+  public polarAreaChartData = this.data1;
+  public polarAreaLegend = true;
+
+  public polarAreaChartType: ChartType = 'polarArea';
+
   constructor(@Inject(MAT_DIALOG_DATA) public data,
     public dialogRef: MatDialogRef<ViewUserDetailsComponent>,
     public service: ReportingUserService) { }
-  data1: any;
+  data2: any;
   ngOnInit() {
     this.fetchAnalytics(this.data.Id);
   }
   fetchAnalytics(id: string) {
     this.service.getUserAnalytics(id).subscribe((res: any) => {
-      this.data1 = res;
+      this.data2 = res;
+      this.data1 = [];
+      this.data1.push(this.data2.HighestScore);
+      this.data1.push(this.data2.LowestScore);
+      this.data1.push(this.data2.Accuracy);
+      this.data1.push(this.data2.AverageScore);
       console.log(this.data1);
+      console.log(this.data2);
     })
+  }
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 }
