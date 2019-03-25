1bcdf95ce1ec275f3219071070e18b6033b5ffae
AnandRoy01 <anandroyyo@gmail.com>
2019-03-25 18:50:31 +0530

    Anand | added get to reporting user

--- WebApp/src/app/reporting-user/reporting-user.component.ts
+++ WebApp/src/app/reporting-user/reporting-user.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportingUserService } from './shared/reporting-user.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
-  selector: 'app-reporting-user',
-  templateUrl: './reporting-user.component.html',
-  styleUrls: ['./reporting-user.component.css']
+	selector: 'app-reporting-user',
+	templateUrl: './reporting-user.component.html',
+	styleUrls: [ './reporting-user.component.css' ]
 })
 export class ReportingUserComponent implements OnInit {
+	Firstname: String;
+	Lastname: String;
+	email: String;
+	Quizzes: any;
+	Questions: any;
+	Tags: any;
+	profileUrl: any;
+	show: boolean = true;
+	constructor(private service: ReportingUserService, private ngxService: NgxUiLoaderService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.ngxService.startBackground('do-background-things');
+		this.ngxService.stopBackground('do-background-things');
+		this.ngxService.startLoader('loader-01');
+		this.profileUrl = localStorage.getItem('imgurl');
+		console.log(this.profileUrl);
+		this.loadUserDetails();
+	}
+	loadUserDetails() {
+		this.service.getUserDetails().subscribe((res: any) => {
+			this.Firstname = res.FirstName;
+			this.Lastname = res.LastName;
+			this.email = res.Email;
+		});
+	}
 }
--- WebApp/src/app/reporting-user/shared/reporting-user.service.ts
+++ WebApp/src/app/reporting-user/shared/reporting-user.service.ts
@@ -3,21 +3,24 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ReportingUserService {
+	rootUrl = environment.apiURl;
 
-  rootUrl = environment.apiURl;
+	constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getTagAnalytics() {
-    return this.http.get(this.rootUrl + "ReportingUser/AnalyticsByTag");
-  }
-  getAllUsers() {
-    return this.http.get(this.rootUrl + 'User/GetUserAll');
-  }
-  getUserAnalytics(id: string) {
-    return this.http.get(this.rootUrl + 'ReportingUser/AnalyticsByUser/' + id);
-  }
+	getTagAnalytics() {
+		return this.http.get(this.rootUrl + 'ReportingUser/AnalyticsByTag');
+	}
+	getAllUsers() {
+		return this.http.get(this.rootUrl + 'User/GetUserAll');
+	}
+	getUserAnalytics(id: string) {
+		return this.http.get(this.rootUrl + 'ReportingUser/AnalyticsByUser/' + id);
+	}
+	getUserDetails() {
+		console.log(localStorage.getItem('email'));
+		return this.http.get(this.rootUrl + 'GetUserDetails?email=' + localStorage.getItem('email'));
+	}
 }
