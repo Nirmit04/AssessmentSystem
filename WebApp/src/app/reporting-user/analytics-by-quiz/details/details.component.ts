e6f49e9e5c3617f60707dcd1200011c312310a76
MeghanaKSrinivas <sreemeghana.k@nineleaps.com>
2019-03-26 14:07:19 +0530

    quiz analytics graph

--- WebApp/src/app/reporting-user/analytics-by-quiz/details/details.component.ts
+++ WebApp/src/app/reporting-user/analytics-by-quiz/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ReportingUserService } from '../../shared/reporting-user.service';
+import { SingleDataSet, Label } from 'ng2-charts';
+import { ChartType, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-details',
@@ -9,6 +11,15 @@ import { ReportingUserService } from '../../shared/reporting-user.service';
 })
 export class DetailsComponent implements OnInit {
   quizData: any;
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = ['Highest-Score', 'Lowest Score', 'Average-Score', 'Number of Schedules'];
+  public pieChartData: any[]
+  public pieChartLegend: boolean;
+
+  public pieChartType: ChartType = 'pie';
+  chartData: any[];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data,
     public dialogRef: MatDialogRef<DetailsComponent>,
@@ -22,7 +33,13 @@ export class DetailsComponent implements OnInit {
   getDetails(id: string) {
     this.service.getQuizAnalysis(id).subscribe((res: any) => {
       this.quizData = res;
-      console.log('i am here');
+      this.chartData = [];
+      this.chartData.push(this.quizData.HighestScore);
+      this.chartData.push(this.quizData.LowestScore);
+      this.chartData.push(this.quizData.AverageMarks);
+      this.chartData.push(this.quizData.NoOfQuiz);
+      this.pieChartData = this.chartData;
+      // console.log('i am here');
       console.log(this.quizData);
     })
   }
