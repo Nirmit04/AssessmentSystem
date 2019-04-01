b165beb2dbfabb5f54114a09d1ef2f49cc9a20f7
Nirmit04 <nirmit.agarwal@nineleaps.com>
2019-04-01 17:36:56 +0530

    Nirmit|Bug Fix in Create tag

--- WebApp/src/app/content-creator/tag/createtag/createtag.component.ts
+++ WebApp/src/app/content-creator/tag/createtag/createtag.component.ts
@@ -4,6 +4,7 @@ import { TagModel } from '../../shared/tags.model';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { forEach } from '@angular/router/src/utils/collection';
 
 
 @Component({
@@ -16,7 +17,9 @@ export class CreatetagComponent implements OnInit {
 
 	public model: TagModel;
 	userId = "";
+	existingTags: TagModel[];
 	Option: string = '';
+	tagExists = false;
 
 	constructor(
 		@Inject(MAT_DIALOG_DATA) public data,
@@ -34,6 +37,10 @@ export class CreatetagComponent implements OnInit {
 			this.Option = 'Update';
 			this.service.tagForm = this.data;
 		}
+		this.service.retrieveSubjects().subscribe((res: any) => {
+			this.existingTags = res as TagModel[];
+			console.log(this.existingTags);
+		});
 	}
 
 	resetForm(form?: NgForm) {
@@ -52,7 +59,20 @@ export class CreatetagComponent implements OnInit {
 			this.toastr.success('Inserted successfully');
 			this.resetForm(form);
 			this.dialogRef.close('Inserted');
-		});;
+		});
 	}
 
+	check_avail(name1: NgForm) {
+		for (let tag of this.existingTags) {
+			if (tag.Name.toString().toLowerCase() === name1.value.toString().toLowerCase()) {
+				this.tagExists = true;
+				break;
+			}
+			else {
+				this.tagExists = false;
+			}
+		}
+
+		console.log(this.tagExists);
+	}
 }
