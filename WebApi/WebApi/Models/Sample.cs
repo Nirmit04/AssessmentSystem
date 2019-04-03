c28cd558bab088812a50b8c20d7d6059fb98fd74
Mandeep-jain <mandeep.jain@nineleaps.com>
2019-04-03 17:30:10 +0530

    Mandeep | employee sample

---
+++ WebApi/WebApi/Models/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Sample
+    {
+        public string designation { get; set; }
+        public string companyname { get; set; }
+        public string jd{ get; set; }
+    }
+}
