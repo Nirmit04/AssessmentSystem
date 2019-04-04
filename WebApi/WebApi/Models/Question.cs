b783c3ed50f40e1203b85f2a8b967085bc3e6dca
abhijeetanandshah6997 <abhijeet.shah@nineleaps.com>
2019-04-04 12:32:21 +0530

    Abhijeet | Sample Image Uploader & Post Question with Type

--- WebApi/WebApi/Models/Question.cs
+++ WebApi/WebApi/Models/Question.cs
@@ -18,6 +18,7 @@ namespace WebApi.Models
         public decimal Marks { get; set; }
         public string ImageName { get; set; }
         public string Difficulty { get; set; }
+        public string QuestionType { get; set; } = "Non-Mock";
 
         [ForeignKey("Subject")]
         public int SubjectId { get; set; }
