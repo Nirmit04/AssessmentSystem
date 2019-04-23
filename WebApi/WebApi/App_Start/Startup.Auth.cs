161df1880507f7fff5c79b8087830115adfa2486
abhijeetanandshah6997 <abhijeet.shah@nineleaps.com>
2019-04-23 14:28:47 +0530

    Abhijeet | Latest Updated

--- WebApi/WebApi/App_Start/Startup.Auth.cs
+++ WebApi/WebApi/App_Start/Startup.Auth.cs
@@ -8,8 +8,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
-using WebApi.Providers;
 using WebApi.Models;
+using WebApi.Providers;
 
 namespace WebApi
 {
