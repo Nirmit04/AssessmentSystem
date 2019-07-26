227955a0cbcd522525264f8eff71a56d8101d4a4
AnandRoy01 <anandroyyo@gmail.com>
2019-07-26 10:10:42 +0530

    anand| sonar implementation

--- WebApp/src/environments/environment.ts
+++ WebApp/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
 	production: false,
-	apiURl: ' http://81d6fe76.ngrok.io/api/',
-	imgURl: ' http://81d6fe76.ngrok.io/Images/'
+	apiURl: 'http://192.168.1.141/webapiv2/api/',
+	imgURl: 'http://192.168.1.141/webapiv2/Images/'
 };
 
 /*
